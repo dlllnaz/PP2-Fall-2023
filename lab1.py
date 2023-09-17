06ba156b7500b75e43a441d634c650d585ba58d9
dlllnaz <144439710+dlllnaz@users.noreply.github.com>
2023-09-17 21:10:09 +0600

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,41 @@
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#print(a + b + c)
+
+#b = int(input())
+#h = int(input())
+#a = (0.5 * b * h)
+#print(a)
+
+#n = int(input())
+#k = int(input())
+# Выводите результат через print()
+#print(k // n)
+#print(k% n)
+
+#n= int(input())
+#hours=n%(60*24)//60
+#min=n%60
+#print(hours,min)
+
+#n = input()
+#print("Hello, " + n + "!")
+
+#n = int(input())
+#k = n - 1
+#p = n + 1
+#print(" The next number for the number " + str(n) + " is " + str(p))
+#print(" The previous number for the number " +str(n) + " is " + str(k))
+
+#a = int(input())
+#b = int(input())
+#c = int(input())
+#print(a // 2 + b // 2 + c // 2 + a % 2 + b % 2 + c % 2)
+
+#a = int(input())
+#b = int(input())
+#l = int(input())
+#N = int(input())
+#Length = (((N*2)-1)*a) + (((N-1)*b)*2) + (2*l)
+#print(Length)
